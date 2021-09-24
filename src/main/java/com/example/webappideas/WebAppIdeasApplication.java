45a1eb0a32baa6b90516dd808a4060e61dc1e3f2
Sergio Alexander Davila <78042215+sergio-davila@users.noreply.github.com>
2021-09-23 20:41:40 -0500

    feat: created a gitignore

---
+++ src/main/java/com/example/webappideas/WebAppIdeasApplication.java
@@ -0,0 +1,13 @@
+package com.example.webappideas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAppIdeasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebAppIdeasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/webappideas/WebAppIdeasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webappideas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebAppIdeasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
