da13a0bbe7b4ac33116ada25d0deec173e66e961
Sergio Alexander Davila <78042215+sergio-davila@users.noreply.github.com>
2021-09-26 01:35:09 -0500

    feat: finished racers seeder script

--- src/main/resources/static/SQL/seeder.sql
+++ src/main/resources/static/SQL/seeder.sql
@@ -11,4 +11,17 @@ VALUES (0, 4, 3, 5, 5, 0, 0, 0, 0, 10, 10, 10, 10),
        (2, 4, 1, 5, 4, 2, 2, 2, 2, 8, 8, 8, 8),
        (3, 4, 3, 4, 3, 3, 3, 3, 3, 7, 7, 7, 7),
        (3, 4, 2, 3, 3, 4, 4, 4, 4, 6, 6, 6, 6),
-       (4, 3, ),
+       (4, 3, 3, 3, 3, 5, 5, 5, 5, 5, 5, 5, 5),
+       (5, 3, 7, 1, 3, 5, 5, 5, 5, 5, 5, 5, 5),
+       (6, 2, 4, 2, 2, 6, 6, 6, 6, 4, 4, 4, 4),
+       (6, 2, 5, 1, 2, 6, 6, 6, 6, 5, 5, 5, 5),
+       (10, 1, 8, 1, 1, 8, 8, 8, 8, 3, 3, 3, 3),
+       (7, 1, 9, 3, 2, 7, 7, 7, 7, 3, 3, 3, 3),
+       (8, 1, 10, 0, 1, 9, 9, 9, 9, 2, 2, 2, 2),
+       (9, 0, 5, 1, 0, 10, 10, 10, 10, 1, 1, 1, 1),
+       (10, 0, 6, 0, 0, 10, 10, 10, 10, 0, 0, 0, 0);
+
+TRUNCATE kart_bodies;
+
+INSERT INTO kart_bodies (weight, acceleration, on_road_attraction, off_road_traction, mini_turbo, ground_speed, water_speed, anti_g_speed, air_speed, ground_handling, water_handling, anti_g_handling, air_handling)
+VALUES (),
