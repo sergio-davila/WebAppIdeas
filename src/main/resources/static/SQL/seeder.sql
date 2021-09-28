e16a3d1ac2076fe103b04a86fcca5650000a7f84
Sergio Alexander Davila <78042215+sergio-davila@users.noreply.github.com>
2021-09-28 01:42:24 -0500

    continued seeder work

--- src/main/resources/static/SQL/seeder.sql
+++ src/main/resources/static/SQL/seeder.sql
@@ -42,4 +42,8 @@ VALUES (2, 4, 3, 3, 4, 3, 3, 3, 3, 3, 2, 3, 3),
 TRUNCATE tires;
 
 INSERT INTO tires (weight, acceleration, on_road_attraction, off_road_traction, mini_turbo, ground_speed, water_speed, anti_g_speed, air_speed, ground_handling, water_handling, anti_g_handling, air_handling)
-VALUES (),
+VALUES (2, 4, , 5, 3, 2, 3, 2, 3, 3, 3, 3, 3),
+       (4, 2, , 7, 2, 2, 2, 2, 1, 0, 1, 0, 1),
+       (0, 6, , 4, 6, 0, 3, 0, 3, 4, 4, 4, 4),
+       (2, 2, , 1, 2, 3, 2, 4, 2, 4, 4, 3, 4),
+       (3, 1, ),
